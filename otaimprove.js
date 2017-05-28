7ef41a56698dd408323d5567b4bfcd5e3e50fc53
no no <123io4p2u342349u823492u8342u34upo34u23wwe@1239482314jw.com>
2017-05-28 18:29:35 -0500

    Cleaning.

--- otaimprove.js
+++ otaimprove.js
@@ -3,10 +3,9 @@
 // @namespace   otaimprove
 // @description cool things for ota (and tinyboard, and probably vichan)
 // @include     https://ota-ch.com/jp/*
-// @version     0.0.1
+// @version     0.0.2
 // @grant       none
 // ==/UserScript==
-// todo: cleanup element creation, use innerHTML w/ template strings.
 // todo: refactor
 
 ;(() => {
@@ -15,9 +14,11 @@
 
   class OtaImprove {
     constructor () {
-     // todo: fixme debug false
+      // todo: fixme debug false
       this.DEBUG = true
       this.hiddenThreads = JSON.parse(window.localStorage.getItem(HIDDEN_THREADS)) || []
+      // kill potential duplicate ids.
+      this.hiddenThreads = Array.from(new Set(this.hiddenThreads))
       this.visibleThreads = []
 
       this.createDebugMenu()
@@ -28,21 +29,20 @@
     createDebugMenu () {
       if (!this.DEBUG) { return }
 
-      let debugLink = document.createElement('div')
-      debugLink.style = 'color: black; font-size: 28px; float: right; box-shadow: 0px 0px 30px 0px rgba(127, 0, 127, 0.67);'
-      debugLink.innerText = 'Debug'
+      let debugLink = document.createElement('button')
+      debugLink.style = 'float: right;'
+      debugLink.innerText = 'Debug OtaImprove'
       document.querySelector('.boardlist').appendChild(debugLink)
 
       let debugElement = document.createElement('div')
       debugElement.style = 'display:none'
-      let otaImproveElement = document.createElement('div')
-      otaImproveElement.innerText = JSON.stringify(this)
+      // simple data dumps that don't need a listener should go here.
+      debugElement.innerHTML = `<div>${JSON.stringify(this)}</div><br>`
+
       let localStorageClear = document.createElement('button')
       localStorageClear.innerText = 'Clear hidden threads'
 
       document.querySelector('.boardlist').appendChild(debugElement)
-
-      debugElement.appendChild(otaImproveElement)
       debugElement.appendChild(localStorageClear)
 
       debugLink.addEventListener('click', () => {
@@ -56,37 +56,42 @@
       })
     }
 
-  // todo narrow scope so this can be run alongside  fn threadHider(), on startup after reading hidden threads.
-  // todo: this should create an array of threads on the current page so it doesnt do dom searches
-  //   for threads in localStorage that aren't on the current page
+    // todo: this should go : make a button, is this in hidden threads? hide it, else we're done.
+    // todo narrow scope so this can be run alongside  fn threadHider(), on startup after reading hidden threads.
     createHideButtons () {
+      // the form HTML element containing all the thread <div>'s
       let threadRoot = document.querySelector('form[name="postcontrols"]')
       let threadCollection = threadRoot.children
       let visibleThreads = []
 
       let hideButton = document.createElement('a')
       hideButton.innerText = 'Hide'
-      hideButton.attributes['href'] = '#'
+      // Unfortunately since some stylesheets(Miku at least) use a a:link pseudo-class to style buttons it can't
+      // always look like [Reply] link button.  don't set an href since it rockets you to the top of the page.
       hideButton.className = HIDE_BTN_CLASS
-    // 0th element is a hidden input field
+      // 0th element is a hidden input field, last two elements are delete fields.
       for (let i = 0; i < threadCollection.length; i++) {
         let thread = threadCollection.item(i)
-        visibleThreads.push(thread.id)
-      // filter the leading hidden input element, and trailing delete elements.
-      // todo: might be more clear to just !idstrcontains('thread'), and likewise for 'post'
-        if (thread.tagName !== 'DIV' || !thread.classList || thread.classList.length !== 0) {
+        // filter the leading hidden input element, and trailing delete elements.
+        // thread elements have an id of the form 'thread_#####'
+        if (!thread.id || !thread.id.includes('thread')) {
+           // its not a thread element, skip it
           continue
         }
+        visibleThreads.push(thread.id)
 
+        // intro element contains the checkbox, name, date string, post number, etc.
         let introElement = thread.querySelector('.intro')
         let uniqueHideButton = hideButton.cloneNode(true)
         uniqueHideButton.addEventListener('click', () => {
           let threadId = thread.id
           console.log('clicked', threadId)
+          // update the array of hiddenThreads, update the localStore, and then hide the thread.
           this.hiddenThreads.push(threadId)
           window.localStorage.setItem(HIDDEN_THREADS, JSON.stringify(this.hiddenThreads))
-          this.hideThread(thread, threadId, false)
+          this.hideThread(thread, threadId)
         })
+
         introElement.appendChild(uniqueHideButton)
       }
 
@@ -132,7 +137,7 @@
         this.hideThread(parent, filteredThreadsToHide[i], false)
       }
     }
-}
+  }
 
   document.onreadystatechange = function () {
     if (!window.localStorage) {
